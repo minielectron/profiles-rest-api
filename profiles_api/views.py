0665168ad3fd20853c3cd83c4e59bfe10bdab388
Prakash <prakash009Kp@gmail.com>
2021-09-21 20:53:08 +0530

    Add API view methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,33 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloViewSerializer
 
     def get(self, request, format=None):
         output = 'hello world!'
         return(Response({'message':'This is demo message', 'output':output}))
+
+    def post(self, request):
+        """Creates a post request and validate the input request using serializers"""
+        
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'DELETE'})
