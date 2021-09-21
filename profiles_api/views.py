ad7a0eea138774f2740a6e9b443622e47c1b5109
Prakash <prakash009Kp@gmail.com>
2021-09-21 15:02:33 +0530

    Add the get api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        output = 'hello world!'
+        return(Response({'message':'This is demo message', 'output':output}))
